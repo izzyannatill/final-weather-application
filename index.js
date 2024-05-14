8aec08d182c378cc2eac22c9e5eb69269ff2391c
izzyannatill <izzyannatill@gmail.com>
2024-05-14 19:07:52 +0100

    Made temp work by adding inner HTML code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector(".current-city");
   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
